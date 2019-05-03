18e36fde37bc030a8e6d1c8d98ae4ef3b0f4baa2
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-05-03 20:32:42 +0900

    passwordに関するテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name:"Example User",email:"user@example.com",
-                     password: "foobar",password_confirmation:"foobar")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -21,7 +21,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should not be too long" do
-    @user.name = "a"*51
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 
@@ -35,7 +35,7 @@ class UserTest < ActiveSupport::TestCase
                          first.last@foo.jp alice+bob@baz.cn foo@bar..com]
     valid_addresses.each do |valid_address|
       @user.email = valid_addresses
-      assert_not @user.valid?,"#{valid_address.inspect} should be valid"
+      assert_not @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
 
@@ -50,6 +50,16 @@ class UserTest < ActiveSupport::TestCase
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
-    assert_equal mixed_case_email.downcase,@user.reload.email
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present(nonblock)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
