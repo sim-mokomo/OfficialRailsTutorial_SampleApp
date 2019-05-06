aef59b09ca32d7b7686cdb0a9098d55d2af2aec6
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-05-06 19:20:45 +0900

    Create user edit page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
 
   def remember(user)
     user.remember
-    cookies.permanent.signed[:user_id] = user_id
+    cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
